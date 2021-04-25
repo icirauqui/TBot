952c4fb5c1ad32464d432724f8310bd66dd9f17f
icirauqui <icirauqui@gmail.com>
2021-04-25 22:24:23 +0200

    rdy

---
+++ LubuntuShare.sh
@@ -0,0 +1,7 @@
+cp TBot6.py /run/user/1000/gvfs/smb-share:server=wyse.local,share=share/TBot6
+cp balance.json /run/user/1000/gvfs/smb-share:server=wyse.local,share=share/TBot6
+cp data.json /run/user/1000/gvfs/smb-share:server=wyse.local,share=share/TBot6
+cp eqbalance.json /run/user/1000/gvfs/smb-share:server=wyse.local,share=share/TBot6
+cp pairs.csv /run/user/1000/gvfs/smb-share:server=wyse.local,share=share/TBot6
+cp trades.json /run/user/1000/gvfs/smb-share:server=wyse.local,share=share/TBot6
+
--- TBot6.py
+++ TBot6.py
@@ -3,13 +3,18 @@ import sys
 import krakenex
 import json
 import time
+import math
+import datetime
 
 import pandas as pd
 
-from tbot6_mysql import *
-
 api_delay = 1
+cryptos_not_owned = 2
 
+blive = True
+
+sema = 1
+lema = 2
 
 
 
@@ -23,7 +28,7 @@ def api_update_nonce():
     fnonce.close()
 
     k.nonce = nonce+1
-    print("NONCE =",k.nonce)
+    #print("NONCE =",k.nonce)
     time.sleep(api_delay)
 
 
@@ -50,7 +55,7 @@ def api_get_balance():
         ret = {'ZEUR':'0.0000'}
 
     save_balance(ret['result'])
-    return ret['result']['ZEUR']
+    return ret['result']
 
 
 def get_balance():
@@ -68,16 +73,29 @@ def save_balance(data):
 
 def update_balance(amount,name,price,sold):
     balance = get_balance()
-    if sold:
-        balance.pop(name[:-4],None)
+    if sold:       
+        if name[4:] == 'ZEUR':
+            balance.pop(name[:-4],None)
+        else:
+            balance.pop(name[:-3],None)
         balance['ZEUR'] = str(float(balance['ZEUR']) + amount*price)
     else:
         balance['ZEUR'] = str(float(balance['ZEUR']) - amount*price)
-        balance[name[:-4]] = str(amount)
+        if name[4:] == 'ZEUR':
+            balance[name[:-4]] = str(amount)
+        else:
+            balance[name[:-3]] = str(amount)
     save_balance(balance)
     return balance
 
 
+def get_available_funds():
+    balance = get_balance()
+    money = float(balance['ZEUR'])
+    cryptos_not_owned = len(pairs) - (len(balance)-1)
+    funds = money/cryptos_not_owned
+    funds = rounddown(funds,8)
+    return funds
 
 
 def api_get_ticker(asset=''):
@@ -127,7 +145,8 @@ def api_get_ticker_ohlc(asset,since):
 
     data = {}
     data['nonce'] = k.nonce
-    data['pair'] = asset[1:len(asset)-4] + "EUR"
+    #data['pair'] = asset[1:len(asset)-4] + "EUR"
+    data['pair'] = asset
     if since > '':
         data['since'] = since
 
@@ -137,15 +156,38 @@ def api_get_ticker_ohlc(asset,since):
     }
 
     api_update_nonce()
+    #print(asset)
     try:
         k.response = k.session.post(url, data = data, headers = headers, timeout = 2)
         resp = k.response.json()['result']
-        asset1 = asset[1:len(asset)-4] + 'EUR'
-        resp[asset] = resp.pop(asset1)
-        return resp
-        #return k.response.json()['result'][asset]
+        #asset1 = asset[1:len(asset)-4] + 'EUR'
+        #resp[asset] = resp.pop(asset1)
+        return resp[asset]
     except:
-        return {asset: [[0, '0.0000', '0.0000', '0.0000', '0.0000', '0.0000', '0.0000', 31]], 'last': 0}
+        return [[0, '0.0000', '0.0000', '0.0000', '0.0000', '0.0000', '0.0000', 31]]
+
+
+def api_get_ticker_ohlc_test(asset,since):
+    urlpath = '/' + k.apiversion + '/public/OHLC'
+    url = k.uri + urlpath
+
+    data = {}
+    data['nonce'] = k.nonce
+    #data['pair'] = asset[1:len(asset)-4] + "EUR"
+    data['pair'] = asset
+    if since > '':
+        data['since'] = since
+
+    headers = {
+        'API-Key'  : k.key,
+        'API-Sign' : k._sign(data,urlpath)
+    }
+
+    api_update_nonce()
+    k.response = k.session.post(url, data = data, headers = headers, timeout = 2)
+    resp = k.response.json()['result']
+    return resp[asset]
+
 
 
 def get_purchasing_price(name):
@@ -168,7 +210,7 @@ def api_get_trades(asset):
     }
 
     k.response = k.session.post(url, data = data, headers = headers, timeout = 1)
-    print(k.response.status_code,"  ",k.response.json())
+    #print(k.response.status_code,"  ",k.response.json())
 
     api_update_nonce()
     return k.response.json()
@@ -200,7 +242,8 @@ def make_crypto_data(data):
             'high'  : [],
             'low'   : [],
             'close' : [],
-            'prices': []
+            'prices': [],
+            'ema'   : []
         }
     return data
 
@@ -208,8 +251,6 @@ def make_crypto_data(data):
 def save_crypto_data(data):
     with open('data.json','w') as f:
         json.dump(data,f,indent=4)
-
-
 
 
 def load_trades():
@@ -231,23 +272,26 @@ def bot(since, k, pairs):
             trades = load_trades()
             
             crypto_data = api_get_ticker_ohlc(pair,since)
-            print(crypto_data)
-            if len(trades[pair]) > 0:
-                if trades[pair][-1]['sold'] or trades[pair][-1]==None:
-                    # Check if we should buy
+            #print(crypto_data)
+            if len(crypto_data)>1:
+                if len(trades[pair]) > 0:
+                    if trades[pair][-1]['sold'] or trades[pair][-1]==None:
+                        # Check if we should buy
+                        check_data(pair,crypto_data,True)
+                    if trades[pair][-1]['bought']:
+                        # Check if we should sell
+                        check_data(pair,crypto_data,False)
+                else:
                     check_data(pair,crypto_data,True)
-                if trades[pair][-1]['bought']:
-                    # Check if we should sell
-                    check_data(pair,crypto_data,False)
-            else:
-                check_data(pair,crypto_data,True)
-        time.sleep(20)
+            save_eq_balance_1(pair,crypto_data)
+            time.sleep(round(((60-len(pairs))/len(pairs))/2))
 
 
 def check_data(name, crypto_data, should_buy):
     high = 0
     low = 0
     close = 0
+    #print(len(crypto_data))
     for b in crypto_data[-100:]:
         if b not in mva[name]['prices']:
             mva[name]['prices'].append(b)
@@ -255,9 +299,17 @@ def check_data(name, crypto_data, should_buy):
         low += float(b[3])
         close += float(b[4])
 
+    df = pd.DataFrame(crypto_data)
+    df['EMA1'] = df.iloc[:,4].ewm(span=1,adjust=False).mean()
+    df['EMA2'] = df.iloc[:,4].ewm(span=2,adjust=False).mean()
+    df['EMA3'] = df.iloc[:,4].ewm(span=5,adjust=False).mean()
+
     mva[name]['high'].append(high/100)
     mva[name]['low'].append(low/100)
     mva[name]['close'].append(close/100)
+    mva[name]['ema1'] = df['EMA1'][-100:].to_list()
+    mva[name]['ema2'] = df['EMA2'][-100:].to_list()
+    mva[name]['ema3'] = df['EMA3'][-100:].to_list()
 
     save_crypto_data(mva)
 
@@ -269,7 +321,7 @@ def check_data(name, crypto_data, should_buy):
 
 def try_buy(data,name,crypto_data):
     # Analyse data to see if it is a good oportunity to buy
-    make_trade = check_opportunity(data,name,False,True)
+    make_trade = check_opportunity_ema(data,name,False,True)
     if make_trade:
         buy_crypto(crypto_data,name)
 
@@ -281,27 +333,64 @@ def buy_crypto(crypto_data,name):
     # Find what we can buy for
     price = float(crypto_data[-1][4])
     funds = get_available_funds()
-    amount = funds * (1/price)
-    balance = update_balance(amount,name,price,False)
-    save_trade(price,name,True,False,amount)
-    print('buy')
+    if (funds>0):
+        amount = rounddown(funds*(1/price),8)
+        balance = update_balance(amount,name,price,False)
+        save_trade(price,name,True,False,amount)
+        print('Buy',amount,name,'at',price,'for',amount*price)
+        print()
+
+
+def api_buy_crypto(name,volume):
+    urlpath = '/' + k.apiversion + '/private/AddOrder'
+    url = k.uri + urlpath
+
+    data = {}
+    data['nonce'] = k.nonce
+    data['pair'] = name
+    data['type'] = 'buy'
+    data['ordertype'] = 'market'
+    data['volume'] = volume
+
+    headers = {
+        'API-Key'  : k.key,
+        'API-Sign' : k._sign(data,urlpath)
+    }
+
+    api_update_nonce()
+    try:
+        k.response = k.session.post(url, data = data, headers = headers, timeout = 2)
+        resp = k.response.json()['result']
+        return resp
+    except:
+        return [[0, '0.0000', '0.0000', '0.0000', '0.0000', '0.0000', '0.0000', 31]]
+
 
    
 def try_sell(data,name,crypto_data):
     # Analyse data to see if it is a good oportunity to sell
-    make_trade = check_opportunity(data,name,True,False)
+    make_trade = check_opportunity_ema(data,name,True,False)
     if make_trade:
         sell_crypto(crypto_data,name)
 
 
 def sell_crypto(crypto_data,name):
-    balance = api_get_balance()
+    balance = get_balance()
     analysis_data = clear_crypto_data(name)
     price = float(crypto_data[-1][4])
-    amount = float(balance[name[:-4]])
+    #cryptos_not_owned += 1
+    if (len(name)==8):
+        amount = float(balance[name[:-4]])
+    else:
+        amount = float(balance[name[:-3]])
     balance = update_balance(amount,name,price,True)
     save_trade(price,name,False,True,amount)
-    print('sell')
+    print('Sell',amount,name,'at',price,'for',amount*price)
+    print()
+
+
+#def api_sell_crypto():
+
 
 
 def clear_crypto_data(name):
@@ -317,6 +406,99 @@ def delete_entries(data,key):
     for entry in data[key][-10:]:
         clean_array.append(entry)
     return clean_array
+
+
+def save_trade(close,name,bought,sold,amount):
+    # Saves trades to json file
+    trade = {
+        'time_stamp'    :   str(int(time.time())),
+        'price_eur'     :   close,
+        'bought'        :   bought,
+        'sold'          :   sold,
+        'amount'        :   amount
+    }
+    print('TRADE:')
+    print(json.dumps(trade,indent=4))
+    trades = load_trades()
+    trades[name].append(trade)
+    with open('trades.json','w') as f:
+        json.dump(trades,f,indent=4)
+
+
+def check_opportunity_ema(data,name,sell,buy):
+    if (len(data['ema2'])>5):
+        above12 = []
+        above13 = []
+        arrema1 = data['ema1'][-5:]
+        arrema2 = data['ema2'][-5:]
+        arrema3 = data['ema3'][-5:]
+
+        for i in range(len(arrema2)):
+            if (arrema1[i] > arrema2[i]):
+                above12.append(1)
+            elif (arrema1[i] < arrema2[i]):
+                above12.append(-1)
+            else: 
+                above12.append(0)
+
+
+        for i in range(len(arrema2)):
+            if (arrema1[i] > arrema3[i]):
+                above13.append(1)
+            elif (arrema1[i] < arrema3[i]):
+                above13.append(-1)
+            else: 
+                above13.append(0)
+
+        k121 = 0
+        k122 = 0
+        for value in above12:
+            k122 = k121
+            k121 = value
+
+        k131 = 0
+        k132 = 0
+        for value in above13:
+            k132 = k131
+            k131 = value
+
+        i = 0
+        i1 = 0
+        iGo = False
+        for value in reversed(above12):
+            if (i==0 and i1==0):
+                if (value==1):
+                    i = value
+                else:
+                    i = -1
+            if (i==1 and i1==0 and value==-1):
+                i1 = value
+        if (i==1 and i1==-1):
+            iGo = True
+
+        j = 0
+        j1 = 0
+        jGo = False
+        for value in reversed(above13):
+            if (j==0 and j1==0):
+                if (value==1):
+                    j = value
+                else:
+                    j = -1
+            if (j==1 and j1==0 and value==-1):
+                j1 = value
+        if (j==1 and j1==-1):
+            jGo = True
+
+        
+    
+        if (sell and k122==1 and k121==-1):
+            return True
+        
+        if (buy and iGo and jGo):
+            return True
+
+    return False
 
 
 def check_opportunity(data,name,sell,buy):
@@ -368,6 +550,121 @@ def check_opportunity(data,name,sell,buy):
 
     return False
 
+def save_eq_balance(pair,data):
+    now = datetime.datetime.now()
+    nowstr = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    balance = get_balance()
+
+    eqbal = {}
+    eqbal1 = {}
+
+    eqbal1[nowstr] = {}
+
+    with open('eqbalance.json','r') as f:
+        try:
+            eqbal = json.load(f)
+        except:
+            eqbal = {}
+
+    try: 
+        eqbal1[nowstr]['ZEUR'] = float(balance['ZEUR'])
+    except:
+        eqbal1[nowstr]['ZEUR'] = 0.0
+
+    datai = data[-1]
+
+    for bal in eqbal:
+        if bal[:-4] in balance:
+            eqbal1[nowstr][bal] = eqbal[bal]
+        if bal[:-3] in balance:
+            eqbal1[nowstr][bal] = eqbal[bal]
+
+    pair1 = pair[:-3]
+    if pair1 in balance:
+        eqbal1[nowstr][pair] = float(datai[4]) * float(balance[pair1])
+
+    pair1 = pair[:-4]
+    if pair1 in balance:
+        eqbal1[nowstr][pair] = float(datai[4]) * float(balance[pair1])
+
+    sumbalance = 0.0
+    for value in eqbal1[nowstr]:
+        if (value=='TOT'):
+            pass
+        else:
+            sumbalance += float(eqbal1[nowstr][value])
+
+    eqbal1[nowstr]['TOT'] = sumbalance
+
+    eqbal[nowstr] = eqbal1[nowstr]
+
+    print(eqbal1)
+
+    with open('eqbalance.json','w') as f:
+        json.dump(eqbal,f,indent=4)
+
+
+
+def save_eq_balance_1(pair,data):
+    now = datetime.datetime.now()
+    nowstr = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    balance = get_balance()
+
+    eqbal = {}
+    with open('eqbalance.json','r') as f:
+        try:
+            eqbal = json.load(f)
+        except:
+            eqbal = {}
+
+    try:
+        eqbal[nowstr] = eqbal[list(eqbal.keys())[-1]]
+    except:
+        eqbal[nowstr] = {}
+
+    datai = data[-1]
+    for bal in balance:
+        if (bal=='ZEUR'):
+            eqbal[nowstr]['ZEUR'] = float(balance['ZEUR'])
+        if (bal==pair[:-3]):
+            eqbal[nowstr][pair[:-3]] = float(datai[4]) * float(balance[bal])
+        if (bal==pair[:-4]):
+            eqbal[nowstr][pair[:-4]] = float(datai[4]) * float(balance[bal])
+
+    try:
+        eqbal[nowstr].pop('TOT')
+    except:
+        pass
+
+    keys2remove = []
+    for bal in eqbal[nowstr]:
+        if bal not in balance:
+            keys2remove.append(bal)
+
+    for bal in keys2remove:
+        eqbal[nowstr].pop(bal)
+
+    sumbalance = 0.0
+    for value in eqbal[nowstr]:
+        if (value=='TOT'):
+            pass
+        else:
+            sumbalance += float(eqbal[nowstr][value])
+    eqbal[nowstr]['TOT'] = sumbalance
+
+    print(nowstr,eqbal[nowstr])
+
+    with open('eqbalance.json','w') as f:
+        json.dump(eqbal,f,indent=4)
+
+
+
+
+def rounddown(number,decimals):
+    return math.floor(number*(10**decimals))/(10**decimals)
+
 
 
 if __name__ == '__main__':
@@ -377,4 +674,10 @@ if __name__ == '__main__':
     pairs = get_pairs()
     since = str(int(time.time()-43200))
     mva = load_crypto_data_from_file()
-    #bot(since,k,pairs)
+
+    #ticker = api_get_ticker_ohlc_test('XXBTZEUR',since)
+    #for b in ticker[-100:]:
+    #    print(b)
+    #print(ticker)
+
+    bot(since,k,pairs)
